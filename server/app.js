c5d1877dba259cd642ab7616591bc628ad1759a6
Bohdan Keller <vickas777@gmail.com>
2017-06-12 18:14:53 +0300

    Fix style

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-
 const index = require('./routes/index');
 const events = require('./routes/events');
 
@@ -33,18 +32,18 @@ app.use('/', index);
 app.use('/events', events);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') !== 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.sendFile('index.html');
 });
-
 
 module.exports = router;
